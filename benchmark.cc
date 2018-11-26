4af1dbc4bb4316ddd74df17f3c4154133bc59622
Mercy Bhakta <mercybhakta@yahoo.com>
2018-11-26 13:57:08 -0800

    Create benchmark.cc

---
+++ benchmark.cc
@@ -0,0 +1,14 @@
+ #include <iostream>
+ #include <chrono>
+ 
+ //we need to time requests
+ //begin time
+ //warm up cache
+ //loop through certain number of times, # of set requests, # of get requests
+ //end time 
+ //subtract warm up cache time from total time
+ 
+ void main () 
+ {
+ 
+ }
