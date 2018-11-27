526fe1024fca63f70c8608f44e22ec0bf9f00737
Mercy Bhakta <mercybhakta@yahoo.com>
2018-11-27 11:32:28 -0800

    Update benchmark.cc

--- benchmark.cc
+++ benchmark.cc
@@ -1,14 +1,35 @@
- #include <iostream>
- #include <chrono>
+#include "cache.hh"
+#include <iostream>
+#include <string>
+#include <stdlib.h> //might need this
+#include <chrono>
  
+using namespace std;
+using namespace std::chrono;
+
+void warmup_cache() 
+{
+ 
+}
+
+void run_benchmark()
+{
  //we need to time requests
+ struct timespec t0, t1; //why is t0 like this??
+ 
+  //warm up cache
+ warmup_cache();
+ 
  //begin time
- //warm up cache
+ clock_gettime(CLOCK_MONOTONIC_RAW, &t0);
+
  //loop through certain number of times, # of set requests, # of get requests
- //end time 
- //subtract warm up cache time from total time
+ 
+ //end time
+ clock_gettime(CLOCK_MONOTONIC_RAW, &t1);
+ const double time_span = (NSECS_IN_SEC * (t1.tv_sec - t0.tv_sec) + (t1.tv_nsec - t0.tv_nsec))
  
  void main () 
  {
- 
+  run_benchmark();
  }
