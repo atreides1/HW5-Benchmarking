32c2895349806f16ceb9ee1a0b139423537c0e99
vagrant <schlotts@reed.edu>
2018-12-01 21:22:11 +0000

    Updated with Monica's rec'd changes

--- cache.h
+++ cache.h
@@ -42,14 +42,14 @@ cache_type create_cache(index_type maxmem, evictor_type evictor, hash_func hashe
 // Both the key and the value are to be deep-copied (not just pointer copied).
 // If maxmem capacity is exceeded, sufficient values will be removed
 // from the cache to accomodate the new value.
-void cache_set(cache_type cache, key_type key, val_type val, index_type val_size);
+int cache_set(cache_type cache, key_type key, val_type val, index_type val_size);
 
 // Retrieve a pointer to the value associated with key in the cache,
 // or NULL if not found.
 val_type cache_get(cache_type cache, key_type key, index_type *val_size);
 
 // Delete an object from the cache, if it's still there
-void cache_delete(cache_type cache, key_type key);
+int cache_delete(cache_type cache, key_type key);
 
 // Compute the total amount of memory used up by all cache values (not keys)
 index_type cache_space_used(cache_type cache);
